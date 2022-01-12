b1ba9031018970ba4a520b9e446a11b1b6443a12
Shaarang Singh <shaarangg.singh@gmail.com>
2022-01-12 17:34:43 +0530

    feat: classes-on-route now gives correct response

--- Backend/routes/classes-on-map.js
+++ Backend/routes/classes-on-map.js
@@ -4,7 +4,23 @@ const Build = require("../models/bulding.model");
 
 router.route("/:courseCode").get((req, res) => {
 	const courseCode = req.params.courseCode;
-	Class.find({ courseCode })
+	Class.aggregate([
+		{ $match: { courseCode: courseCode } },
+		{
+			$lookup: {
+				from: "buildings",
+				localField: "building",
+				foreignField: "name",
+				as: "fromBuilding",
+			},
+		},
+		{
+			$replaceRoot: {
+				newRoot: { $mergeObjects: [{ $arrayElemAt: ["$fromBuilding", 0] }, "$$ROOT"] },
+			},
+		},
+		{ $project: { fromBuilding: 0, name: 0 } },
+	])
 		.then((result) => {
 			console.log(result);
 			res.json("Success");
