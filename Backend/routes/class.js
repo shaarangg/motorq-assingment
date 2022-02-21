c4332c286ee33e8b96b05b1188c9e5d05ece0b3e
Shaarang Singh <shaarangg.singh@gmail.com>
2022-02-21 18:29:23 +0530

    fix: /class/:sid

--- Backend/routes/class.js
+++ Backend/routes/class.js
@@ -1,33 +1,55 @@
 const router = require("express").Router();
 const Class = require("../models/class.model");
 const Student = require("../models/student.model");
-var mongoose = require("mongoose");
 // Adds a class to the student
-router.route("/:id").post((req, res) => {
-	const sid = req.params.id;
-	const cid = req.body.id;
-	Student.updateOne({ _id: sid }, { $push: { classes: cid } })
-		.then((result) => {
-			console.log(result);
-			Class.updateOne({ _id: cid }, { $inc: { studentsRegistered: 1 } })
-				.then((classResult) => {
-					console.log(classResult);
-					res.json({ status: "Success" });
-				})
-				.catch((err) => res.status(400).json("Error: " + err));
-		})
-		.catch((err) => res.status(400).json("Error: " + err));
+router.route("/:id").post(async (req, res) => {
+	try {
+		const sid = req.params.id;
+		const cid = req.body.id;
+		const time = req.body.time;
+		let studentClasses = await Student.aggregate([
+			{ $match: { _id: sid } },
+			{
+				$lookup: {
+					from: "classes",
+					localField: "classes",
+					foreignField: "_id",
+					as: "classArray",
+				},
+			},
+			{
+				$project: { classArray: 1 },
+			},
+		]);
+		studentClasses = JSON.parse(JSON.stringify(studentClasses));
+		studentClasses = studentClasses[0].classArray;
+		const classTime = studentClasses.map((cls) => {
+			return cls.time;
+		});
+		if (classTime.includes(time)) {
+			res.json({ success: false, message: "Class Clashing" });
+		} else {
+			await Student.updateOne({ _id: sid }, { $push: { classes: cid } });
+			await Class.updateOne({ _id: cid }, { $inc: { studentsRegistered: 1 } });
+			res.json({ success: true, message: "Class successfully added" });
+		}
+	} catch (e) {
+		res.json({ success: false, message: e });
+	}
 });
 
 // Deletes a class from the student id
-router.route("/:sid/:cid").delete((req, res) => {
-	const { sid, cid } = req.params;
-	Student.updateOne({ _id: sid }, { $pull: { classes: cid } })
-		.then((response) => {
-			console.log(response);
-			res.json({ status: "Success" });
-		})
-		.catch((err) => res.status(400).json("Error: " + err));
+router.route("/:sid/:cid").delete(async (req, res) => {
+	try {
+		const { sid, cid } = req.params;
+		if (!studentId || !classId) {
+			res.json({ success: false, message: "Required fields cannot be empty" });
+		}
+		const student = await Student.updateOne({ _id: sid }, { $pull: { classes: cid } });
+		res.json({ success: true, message: "Class deleted successfully", data: student });
+	} catch (e) {
+		res.json({ success: false, message: e });
+	}
 });
 
 // Gets all the classes of sid
