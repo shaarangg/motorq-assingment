047b7c51cc460545d29b9ab356e36259386de1a9
Shaarang Singh <shaarangg.singh@gmail.com>
2022-02-23 19:08:52 +0530

    feat: Marker Cluster group

--- Frontend/components/MapComp.jsx
+++ Frontend/components/MapComp.jsx
@@ -1,19 +1,48 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import MarkerClusterGroup from "react-leaflet-markercluster";
 L.Icon.Default.imagePath = "../leaflet_images/";
-function MapComp() {
+
+function MapComp({ courseCode }) {
+	const [classes, setClasses] = useState([]);
+	const fetchCycles = async (signal) => {
+		try {
+			const res = await axios.get(`http://localhost:3002/classes-on-map/${courseCode}`);
+			setClasses(res.data.data);
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
+	useEffect(() => {
+		const AbortCont = new AbortController();
+		fetchCycles(AbortCont.signal);
+	}, []);
+
 	return (
-		<MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "400px", width: "100%" }}>
+		<MapContainer center={[12.9, 79.14]} zoom={4} style={{ height: "100%", width: "100%" }}>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
-			<Marker position={[51.505, -0.09]}>
-				<Popup>
-					A pretty CSS3 popup. <br /> Easily customizable.
-				</Popup>
-			</Marker>
+			<MarkerClusterGroup>
+				{classes.map((cls) => {
+					const { _id, location, studentsRegistered, faculty, building, time } = cls;
+					return (
+						<Marker key={_id} position={[location.lat, location.lon]}>
+							<Popup style={{ dispay: "flex", gap: "1rem" }}>
+								<div>Faculty: {faculty}</div>
+								<div>Building: {building}</div>
+								<div>Time: {time}</div>
+								<div>Students Registered: {studentsRegistered}</div>
+							</Popup>
+						</Marker>
+					);
+				})}
+			</MarkerClusterGroup>
 		</MapContainer>
 	);
 }
--- Frontend/pages/map/[courseCode].jsx
+++ Frontend/pages/map/[courseCode].jsx
@@ -1,9 +1,17 @@
 import dynamic from "next/dynamic";
+import Layout from "../../components/Layout";
+import { useRouter } from "next/router";
 function MapPage() {
 	const MapComp = dynamic(() => import("../../components/MapComp"), {
 		ssr: false,
 	});
-	return <MapComp></MapComp>;
+	const router = useRouter();
+	const { courseCode } = router.query;
+	return (
+		<Layout>
+			<MapComp courseCode={courseCode}></MapComp>
+		</Layout>
+	);
 }
 
 export default MapPage;
